cd23ca7d14b2bc3dab38473fd1e13b6e84f9312c
Igor Gromadskiy <gromadskiyigor@gmail.com>
2024-10-31 15:13:18 +0200

    [Add] responsive styles hero section

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,10 +13,12 @@ export default function Header() {
   const closeMenu = () => {
     setIsMenuOpen(false);
     document.body.style.overflow = 'auto';
+    document.body.style.touchAction = 'auto';
   };
   const openMenu = () => {
     setIsMenuOpen(true);
     document.body.style.overflow = 'hidden';
+    document.body.style.touchAction = 'none';
   };
   return (
     <header className={styles.header}>
